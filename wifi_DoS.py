d815349a6b1915e2719553b1cd60d19244b221fd
deeghayu <deefernando6@gmail.com>
2021-07-03 23:06:08 +0530

    Update wifi_DoS.py

--- wifi_DoS.py
+++ wifi_DoS.py
@@ -22,3 +22,5 @@ def check_for_essid(essid, lst): #test if ESSID is already in the list
 if not "SUDO_UID" in os.environ.keys(): #check whether user run with sudo previledges.
 	print('You need root previledges. Try with sudo')
 	exit()
+
+for file_name in os.listdir(): #to remove csv files before exxecuting the script
